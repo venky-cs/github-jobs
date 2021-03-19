cefd1c3e737be8bb681f88c6b94fe92859652200
venkatesh <75250713+venky-cs@users.noreply.github.com>
2021-03-19 18:08:48 +0530

    'create-header-styles'

--- src/component/Header/Header.tsx
+++ src/component/Header/Header.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import './Header.css'
 import Search from './Search'
 
 let title:string = 'Github Jobs'
--- src/component/Header/Search.tsx
+++ src/component/Header/Search.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Search = () => {
     return (
         <div className="search">
-            <input type="text"/>
+            <input type="text" placeholder="Title,Companies,expertise or benefits"/>
             <button>Search</button>
         </div>
     )

0831934b3f4bfe83de2361742b48596f09f39412
venkatesh <75250713+venky-cs@users.noreply.github.com>
2021-03-19 18:22:13 +0530

    'create-footer'

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Header from './component/Header/Header'
 import Main from './component/Main/Main';
+import Footer from './component/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Main />
+      <Footer />
     </div>
   );
 }
---
+++ src/component/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div className="footer">
+            <p>created by venky -devChallenges.io</p>
+        </div>
+    )
+}
+
+export default Footer

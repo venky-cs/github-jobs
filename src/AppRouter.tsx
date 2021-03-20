00b6c37a0bc666f5c928136f0d6da6894789aa76
venkatesh <75250713+venky-cs@users.noreply.github.com>
2021-03-21 01:12:00 +0530

    'fetch-data'

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 import {BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import App from './App'
 import Info from './component/page/Info'
 
+import axios from 'axios'
+let cors = "https://cors-anywhere-venky.herokuapp.com/";
+
 function AppRouter() {
+    const [state,setState] = useState<boolean>(false)
+    useEffect(() => {
+        axios.get(`${cors}https://jobs.github.com/positions.json?description=node`)
+            .then(response => console.log(response.data))
+            .catch(err => console.log(err))
+    }, [state])
+
     return (
         <div>
             <Router>
@@ -33,6 +43,7 @@ function AppRouter() {
 
     function updateState(){
         console.log("fire")
+        setState(prevState => !prevState)
     }
 }
 
